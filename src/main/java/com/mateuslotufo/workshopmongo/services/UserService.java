35b539aa59172f3be7fd6cce2b45342e44ff8223
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-02-16 21:15:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/mateuslotufo/workshopmongo/services/UserService.java
+++ src/main/java/com/mateuslotufo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mateuslotufo.workshopmongo.domain.User;
+import com.mateuslotufo.workshopmongo.dto.UserDTO;
 import com.mateuslotufo.workshopmongo.repositories.UserRepository;
 import com.mateuslotufo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
