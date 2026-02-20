34335502f27af8d348357475e3f70cb9f5bc7da4
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-02-19 21:00:31 -0300

    Consulta simples com query methods

--- src/main/java/com/mateuslotufo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/mateuslotufo/workshopmongo/repositories/PostRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mateuslotufo.workshopmongo.domain.Post;
+import java.util.List;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	public List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
