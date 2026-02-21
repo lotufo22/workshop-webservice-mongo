26d9dd1b8604a6b783a525902cf6853ae3629ecf
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-02-20 22:02:30 -0300

    Consulta com vários critérios

--- src/main/java/com/mateuslotufo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/mateuslotufo/workshopmongo/repositories/PostRepository.java
@@ -1,11 +1,13 @@
 package com.mateuslotufo.workshopmongo.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mateuslotufo.workshopmongo.domain.Post;
-import java.util.List;
 
 
 @Repository
@@ -16,4 +18,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
